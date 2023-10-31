2d7ba4df145b30d881140e5b57f90d3e07a8248b
Humberto Cordioli <hcordioli@gmail.com>
2023-10-31 16:14:26 -0300

    Update 4_📊_charts.py

--- pages/4_📊_charts.py
+++ pages/4_📊_charts.py
@@ -198,7 +198,7 @@ with st.container():
     c_bp_df = st.session_state['outliers_df'].loc[st.session_state['outliers_df']['bxp_outlier'] == False][st.session_state['selected_feature']]
     n_outliers = len(st.session_state['outliers_df'].loc[st.session_state['outliers_df']['bxp_outlier'] == True])
 
-    plot_bxp(os, c_bp_af, whis, n_outliers)
+    #plot_bxp(os, c_bp_af, whis, n_outliers)
     # The 5 numbers
     c_bp_5.table(c_bp_df.describe().apply("{0:.2f}".format))
 
